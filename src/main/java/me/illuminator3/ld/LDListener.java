ceb81d76b828c776b1dfba6c9fc8d5ccecc1bce1
illuminator3 <hardt-j@web.de>
2021-12-05 15:36:00 +0100

    Fix indentation and remove unused imports

--- src/main/java/me/illuminator3/ld/LDListener.java
+++ src/main/java/me/illuminator3/ld/LDListener.java
@@ -1,6 +1,5 @@
 package me.illuminator3.ld;
 
-import com.lunarclient.bukkitapi.nethandler.client.LCPacketGhost;
 import com.lunarclient.bukkitapi.nethandler.client.LCPacketModSettings;
 import com.lunarclient.bukkitapi.nethandler.client.LCPacketStaffModState;
 import com.lunarclient.bukkitapi.nethandler.client.obj.ModSettings;
@@ -11,57 +10,56 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 public class LDListener
     implements Listener
 {
     private static final String[] MOD_IDS = {
-         "one_seven_visuals",
-         "fps",
-         "cps",
-         "skyblockAddons",
-         "toggleSneak",
-         "hypixel_mod",
-         "armorstatus",
-         "keystrokes",
-         "coords",
-         "crosshair",
-         "potioneffects",
-         "directionhud",
-         "waypoints",
-         "scoreboard",
-         "potion_counter",
-         "ping",
-         "motionBlur",
-         "chat",
-         "scrollable_tooltips",
-         "uhc_overlay",
-         "particleMultiplierMod",
-         "cooldowns",
-         "worldedit_cui",
-         "clock",
-         "stopwatch",
-         "memory",
-         "combo",
-         "range",
-         "time_changer",
-         "serverAddressMod",
-         "saturation",
-         "itemPhysic",
-         "itemTrackerChild",
-         "shinyPots",
-         "block_outline",
-         "screenshot",
-         "fov_mod",
-         "textHotKey",
-         "netgraph",
-         "mumble-link",
-         "bossbar",
-         "freelook",
-         "replaymod",
-         "nickHider"
+        "one_seven_visuals",
+        "fps",
+        "cps",
+        "skyblockAddons",
+        "toggleSneak",
+        "hypixel_mod",
+        "armorstatus",
+        "keystrokes",
+        "coords",
+        "crosshair",
+        "potioneffects",
+        "directionhud",
+        "waypoints",
+        "scoreboard",
+        "potion_counter",
+        "ping",
+        "motionBlur",
+        "chat",
+        "scrollable_tooltips",
+        "uhc_overlay",
+        "particleMultiplierMod",
+        "cooldowns",
+        "worldedit_cui",
+        "clock",
+        "stopwatch",
+        "memory",
+        "combo",
+        "range",
+        "time_changer",
+        "serverAddressMod",
+        "saturation",
+        "itemPhysic",
+        "itemTrackerChild",
+        "shinyPots",
+        "block_outline",
+        "screenshot",
+        "fov_mod",
+        "textHotKey",
+        "netgraph",
+        "mumble-link",
+        "bossbar",
+        "freelook",
+        "replaymod",
+        "nickHider"
     }, STAFF_MOD_IDS = {
         "xray",
         "bunny_hop",
